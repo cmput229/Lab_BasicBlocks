77fa7e8e3a45371a0f57b3b5b7cf4736a9f1ef4f
Islam A. Ali <54554220+IslamAAli@users.noreply.github.com>
2021-04-11 19:24:15 -0600

    Update lab file

--- resources/code/lab5.s
+++ resources/code/lab5.s
@@ -2,9 +2,9 @@
 # CMPUT 229 - Lab 5: Basic Blocks
 # ----------------------------------------------------------------------------------------
 # This is a template file for Lab 5 code, please follow the following instructions:
-# 	1) File should be named lab5.s
-#	  2) Your code should go inside the getControlFlow routine.
-#	  3) Please refer to the lab description for the memory and registers assumptions
+#  1) File should be named lab5.s
+#  2) Your code should go inside the getControlFlow routine.
+#  3) Please refer to the lab description for the memory and registers assumptions
 # ----------------------------------------------------------------------------------------
 
 
